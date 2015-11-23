80ed366e6eacf83007db13d2256eaf842c5e8464
danqi <danqiliao@gmail.com>
2015-11-23 00:23:02 -0600

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	#return the full title on a per-page basis
+	def full_title(page_title = '')
+		base_titile = "Ruby on Rails Tutorial"
+		if page_title.empty?
+			base_titile
+		else
+			page_title + " | " + base_titile
+		end
+	end
 end
