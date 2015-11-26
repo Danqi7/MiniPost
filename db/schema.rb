8a6f1eff7dc622f330d7a96ff86540419b9979d9
danqi <danqiliao@gmail.com>
2015-11-25 23:20:58 -0600

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20151124053220) do
 
-# Could not dump table "users" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
